0b75df4ecdc8309175d67b1896a0fdd2e4604926
dotview <lexinquan@gmail.com>
2012-12-24 15:58:36 +0800

    fsl

--- fsl.py
+++ fsl.py
@@ -1,17 +1,19 @@
+﻿#coding:utf-8
+
 import csv
 import json
 from geopy import geocoders  
 
 class formatCSV():
 	"""format csv file, convert address to geo location"""
-	def __init__(self, geo_type='google', file_format='csv'):
+	def __init__(self, geo_type='google', file_format='json'):
 		if geo_type == "google":	
 			self.g = geocoders.Google()  
 		elif geo_type == "mapquest":	
 			self.g =  geocoders.OpenMapQuest("0Fmjtd%7Cluua2d0r2l%2C2w%3Do5-hf7gl")
 		self.rows = []
 		self.file_format = file_format
-		self.FIELDS = ["Institution - Link to Programs","Institution Name","Address1","Address2","City","State","Zip","inst_url","CEO Name","CEO Title","CEO Phone","CEO Fax","CEO Email","DIOI Name","DIOI Title","DIOI Phone","DIOI Fax","DIOIP","DIOI Email" ]  
+		self.FIELDS = ["Reporting Region",	"City",	"State",	"Zip",	"Country",	"FSL Provider" ]  
 	
 	def format(self):
 			self._readCSV()
@@ -23,9 +25,9 @@ class formatCSV():
 					
 	def _readCSV(self):
 			# append new columns
-			self.FIELDS.extend(["FullAddress","lat","lng"])
+			self.FIELDS.extend(["lat","lng"])
 
-			f = open( 'input.csv', 'r' )
+			f = open( 'result_fsl.csv', 'r' )
 			reader = csv.DictReader( f, fieldnames = self.FIELDS )
 			#skip the head row
 			next(reader)
@@ -35,7 +37,7 @@ class formatCSV():
 	def _geoAddress(self):
 			for row in self.rows:
 					if (row["lat"] is None or row["lat"] == ""):
-							address = "%s %s %s" % (row["Address1"], row["City"], row["State"])             
+							address = "%s %s" % (row["Zip"],  row["Country"])             
 							try:                
 									place, (lat, lng) = self.g.geocode(address,False)[0]
 									row["FullAddress"]  = address
@@ -48,7 +50,7 @@ class formatCSV():
 	
 	def _writeCSV(self):
 			# DictWriter  
-			csv_file = open('result.csv', 'wb')  
+			csv_file = open('result_fsl.csv', 'wb')  
 			writer = csv.DictWriter(csv_file, fieldnames=self.FIELDS)  
 			# write header  
 			writer.writerow(dict(zip(self.FIELDS, self.FIELDS)))  
@@ -60,9 +62,16 @@ class formatCSV():
 			
 	
 	def _writeJSON(self):
+			'''
+			for row in self.rows:
+				try:  
+					out = "%s %s" % out,json.dumps(row)
+				except Exception, err:
+					pass
+			'''
 			out = json.dumps( [ row for row in self.rows if  row["lat"] != "" ] )
 			#print out
-			f = open('result.json','wb')
+			f = open('result_fsl.json','wb')
 			f.write(out)
 			f.close()
 
